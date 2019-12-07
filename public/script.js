4ced6df6626ee7ebcd561d493d40bd9bfbd67c1e
Jesper <jesper.lundqvist@gmail.com>
2019-12-07 15:52:41 +0100

    added more animals

--- public/script.js
+++ public/script.js
@@ -9,14 +9,14 @@ const cardsData = [
     { image: "./img/panda.svg", key: "Panda", level: 1 },
     { image: "./img/penguin.svg", key: "Penguin", level: 1 },
     { image: "./img/fox.svg", key: "Fox", level: 1 },
-    { image: "./img/ape.svg", key: "Ape", level: 2 },
-    { image: "./img/bear.svg", key: "Bear", level: 2 },
-    { image: "./img/cow.svg", key: "Cow", level: 2 },
-    { image: "./img/dog.svg", key: "Dog", level: 2 },
-    { image: "./img/donkey.svg", key: "Donkey", level: 2 },
-    { image: "./img/panda.svg", key: "Panda", level: 2 },
-    { image: "./img/penguin.svg", key: "Penguin", level: 2 },
-  
+    { image: "./img/cat.svg", key: "Ape", level: 2 },
+    { image: "./img/whale.svg", key: "Bear", level: 2 },
+    { image: "./img/bat.svg", key: "Cow", level: 2 },
+    { image: "./img/eagle.svg", key: "Dog", level: 2 },
+    { image: "./img/elephant.svg", key: "Donkey", level: 2 },
+    { image: "./img/rabbit.svg", key: "Panda", level: 2 },
+    { image: "./img/dolphin.svg", key: "Penguin", level: 2 },
+
 
 ];
 
@@ -37,7 +37,6 @@ let cardsLevel = [];
 let level;
 let clickLimit;
 let clickCounter = 0;
-let hasLost;
 
 
 
